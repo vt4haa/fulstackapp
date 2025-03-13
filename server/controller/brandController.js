873ae5fdc02d22764fb2e6615efd5f6bbb7dc90f
vt4haa <vt4haa@gmail.com>
2025-03-13 17:29:36 +0500

    for lap

--- server/controller/brandController.js
+++ server/controller/brandController.js
@@ -1,14 +1,15 @@
 const {Brand} = require('../models/models')
 const ApiError = require('../error/apiError')
 class brandController {
+
     async create(req, res) {
-    const {name} = req.body
-    const brand = await Brand.create({name})
-    return res.json(brand)
+        const {name} = req.body
+        const brand = await Brand.create({name})
+        return res.json(brand)
     }
     async getAll(req, res) {
         const brands = await Brand.findAll()
-        return brands
+        return res.json(brands)
     }
 }
 module.exports = new brandController
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const sequelize = require('./db')
 const PORT = process.env.PORT || 5000
 const models = require('./models/models')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./rouds/index')
 const errorHandler = require('./midlleware/ErrorHandlerMiddleware')
 
@@ -11,6 +12,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(fileUpload({}))
 app.use('/api', router)
 
 //Обработка ошибки
